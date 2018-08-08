d9496d42642c151a9a787553a08d7900632aebe5
杨云胜 <yangyunsheng@haier.com>
2018-08-08 18:01:39 +0800

    登录页
    登录页

--- src/main.js
+++ src/main.js
@@ -2,6 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import 'normalize.css/normalize.css'// A modern alternative to CSS resets
+
+// 引入element
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import locale from 'element-ui/lib/locale/lang/en' // lang i18n
+
+import '@/styles/index.scss' // global css
+
+Vue.use(ElementUI, { locale })
+
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/views/layout/components/AppMain.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- section 区域 -->
+  <section class="app-main">
+    <!-- transition Vue 在插入、更新或者移除 DOM 时的过渡效果 -->
+    <transition name="fade" mode="out-in">
+      <!-- <router-view :key="key"></router-view> -->
+      <router-view></router-view>
+    </transition>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'AppMain',
+  computed: {
+    // key() {
+    //   return this.$route.name !== undefined ? this.$route.name + +new Date() : this.$route + +new Date()
+    // }
+  }
+}
+</script>
+
+<style scoped>
+.app-main {
+  /*50 = navbar  */
+  min-height: calc(100vh - 50px);
+  position: relative;
+  overflow: hidden;
+}
+</style>
